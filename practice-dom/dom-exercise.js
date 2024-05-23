ab8a03f136191f0603f85eac7600de670e8d5aea
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-23 16:13:04 +0900

    a

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,11 +14,14 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-
-
+let h2 = document.querySelector('h2#ex42');
+let p =document.createElement('p');
+p.textContent = '写真表と都市の緯度経度のページです';
+p.setAttribute('style', 'text-emphasis: sesame blue');
+h2.insertAdjacentElement('afterend', p); 
 // 練習4-3 写真表作成プログラム
-
-
+let div = document.querySelector('div#phototable');
+let a = document.createElement('img');
 // 練習4-4 箇条書き削除プログラム
 
 
