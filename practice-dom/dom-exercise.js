0616ec6d3ac7f9e1cbbd3820e52be10eb4042625
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-25 15:05:23 +0900

    a

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -49,6 +49,6 @@ for (let n of w) {
 let w1 = document.querySelector('ul#location');
 for (let n of data) {
 	let li = document.createElement('li');
-	li.textContent = n.name+' ... '+lat+ ',' +lng;
+	li.textContent = n.name+lat+lng;
     w1.insertAdjacentElement('beforeend', li);
 	} 
