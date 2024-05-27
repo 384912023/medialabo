654425489d821cd5737e41d09ffedced78c7ea47
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-27 12:22:48 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -1,6 +1,6 @@
 // 答え
 let kotae = Math.floor(Math.random()*10) + 1;
-let span = document.querySelector('span#kaisu');	
+let span = document.querySelector('span#answer');	
 let p = document.createElement('p');
 p.textContent = ('答え（デバッグ用）: ' + kotae);
 span.insertAdjacentElement('beforeend', p);
