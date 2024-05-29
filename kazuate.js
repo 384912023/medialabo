33f02beff5fb1d05e8d9ce426ddf879914d3c496
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-29 22:23:35 +0900

    h

--- kazuate.js
+++ kazuate.js
@@ -1,8 +1,6 @@
 // 答え
 let kotae = Math.floor(Math.random()*10) + 1;
 
-kotae = 4;
-
 let span1 = document.querySelector('span#kaisu');	
 let span2 = document.querySelector('span#answer');	
 let p0 = document.querySelector('p#result');	
