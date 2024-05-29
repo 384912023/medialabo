d635f602602e26f9a3d70245f792dfbbe008d27a
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-29 19:30:51 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -1,13 +1,16 @@
 // 答え
 let kotae = Math.floor(Math.random()*10) + 1;
 let span1 = document.querySelector('span#kaisu');	
-let span = document.querySelector('span#answer');	
+let span2 = document.querySelector('span#answer');	
 let p0 = document.querySelector('p#result');	
 
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
+span1.insertAdjacentElement('beforeend', p1);
+span2.insertAdjacentElement('beforeend', p2);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-//span1.insertAdjacentElement('beforeend', p1);
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -21,14 +24,15 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  p1.textContent = kaisu;
+  p2.textContent = yoso;
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
   let seikai=0;
   kaisu=kaisu+1;
   p0.textContent = kaisu;
-  p0.textContent = ("回目の         予想:"+4);
+  p0.textContent = ("回目の予想:"+4);
   if(kaisu<4){
     if(kotae===4){
       p0.textContent = ("正解です．おめでとう!");
