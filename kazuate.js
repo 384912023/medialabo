70b64bf1cbedc16d5e446b6a8a4c0e1e7d0ede16
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-30 11:51:24 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -22,8 +22,7 @@ c1.addEventListener('click', hantei);
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso =0;
-  yoso = Math.floor(t);
-  //Number(t);
+  yoso = Number(t);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
