6462ffc3cfb6d763e2795e306a37891bdb38b594
G384912023 <238491@st.takushoku-u.ac.jp>
2024-05-30 11:38:44 +0900

    x

--- kazuate.js
+++ kazuate.js
@@ -20,9 +20,9 @@ let c1 = document.querySelector('#print');
 c1.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  Number(t);
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let yoso =0;
+  yoso = Number(t);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
